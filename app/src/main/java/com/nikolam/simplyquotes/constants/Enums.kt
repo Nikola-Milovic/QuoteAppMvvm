ceaa4ada4e3b7e9b41ab08f884bb06a1191527d0
Nikola Milovic <nikolamilovic2001@gmail.com>
2020-03-03 18:27:05 +0100

    Untrack files in .gitignore

--- app/src/main/java/com/nikolam/simplyquotes/constants/Enums.kt
+++
@@ -1,9 +0,0 @@
-package com.nikolam.simplyquotes.constants
-
-
-// Used for checking the Quote Fragments state
-enum class apiState {
-    LOADING,
-    FAILURE,
-    SUCCESS
-}
--- app/src/main/java/com/nikolam/simplyquotes/util/OnQuoteClickListener.kt
+++
@@ -1,9 +0,0 @@
-package com.nikolam.simplyquotes.util
-
-import android.view.View
-import android.widget.PopupMenu
-import com.nikolam.simplyquotes.data.model.Quote
-
-interface OnQuoteClickListener : PopupMenu.OnMenuItemClickListener {
-    fun quoteClicked(quote: Quote, view: View)
-}
